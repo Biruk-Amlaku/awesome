a6383d30ae630dcdfaf9a6d900a49e1418b84523
Brook Amlaku <brookwork24@gmail.com>
2020-03-17 17:37:01 +0300

    Code cleanup

--- color/blue/desktop-config.lua
+++ color/blue/desktop-config.lua
@@ -43,19 +43,6 @@ function desktop:init(args)
 
 	netspeed.style  = {}
 
-	-- -- SSD speed
-	-- ----------------------------------------------------------------------------
-	-- local ssdspeed = { geometry = wgeometry(grid, places.ssdspeed, workarea) }
-
-	-- ssdspeed.args = {
-	-- 	interface = "nvme0n1",
-	-- 	meter_function = system.disk_speed,
-	-- 	timeout   = 2,
-	-- 	label     = "SOLID DRIVE"
-	-- }
-
-	-- ssdspeed.style = beautiful.individual.desktop.speedmeter.drive
-
 	-- HDD speed
 	--------------------------------------------------------------------------------
 	local hddspeed = { geometry = wgeometry(grid, places.hddspeed, workarea) }
@@ -83,19 +70,6 @@ function desktop:init(args)
 
 	cpumem.style = beautiful.individual.desktop.multimeter.cpumem
 
-	-- -- Transmission info
-	-- --------------------------------------------------------------------------------
-	-- local transm = { geometry = wgeometry(grid, places.transm, workarea) }
-
-	-- transm.args = {
-	-- 	topbars    = { num = 8, maxm = 100 },
-	-- 	lines      = { { maxm = 6*1024 }, { maxm = 6*1024 } },
-	-- 	meter      = { async = system.transmission.info, args = { speed_only = true } },
-	-- 	timeout    = 5,
-	-- }
-
-	-- transm.style = beautiful.individual.desktop.multimeter.transmission
-
 	-- Disks
 	--------------------------------------------------------------------------------
 	local disks = { geometry = wgeometry(grid, places.disks, workarea) }
@@ -121,23 +95,6 @@ function desktop:init(args)
 	-- start auto async lmsensors check
 	system.lmsensors:soft_start(sensors_base_timeout)
 
-	-- Temperature indicator
-	--------------------------------------------------------------------------------
-	local thermal = { geometry = wgeometry(grid, places.thermal, workarea) }
-
-	local hdd_smart_check = system.simple_async("smartctl --attributes /dev/sda", "194.+%s(%d+)%s%(.+%)\r?\n")
-
-	thermal.args = {
-		sensors = {
-			-- { meter_function = system.lmsensors.get, args = "cpu", maxm = 100, crit = 75, name = "cpu" },
-			{ async_function = hdd_smart_check, maxm = 60, crit = 45, name = "hdd" },
-			-- { async_function = system.thermal.nvoptimus, maxm = 105, crit = 80, name = "gpu" }
-		},
-		timeout = sensors_base_timeout,
-	}
-
-	thermal.style = beautiful.individual.desktop.singleline.thermal
-
 
 	-- Initialize all desktop widgets
 	--------------------------------------------------------------------------------
@@ -147,12 +104,12 @@ function desktop:init(args)
 	cpumem.body   = redflat.desktop.multimeter(cpumem.args, cpumem.style)
 	-- transm.body   = redflat.desktop.multimeter(transm.args, transm.style)
 	disks.body    = redflat.desktop.multiline(disks.args, disks.style)
-	thermal.body  = redflat.desktop.singleline(thermal.args, thermal.style)
+	-- thermal.body  = redflat.desktop.singleline(thermal.args, thermal.style)
 
 	-- Desktop setup
 	--------------------------------------------------------------------------------
 	-- local desktop_objects = { netspeed, hddspeed, ssdspeed, transm, cpumem, disks, thermal }
-	local desktop_objects = { netspeed, hddspeed, cpumem, disks, thermal }
+	local desktop_objects = { netspeed, hddspeed, cpumem, disks }
 
 	if not autohide then
 		redflat.util.desktop.build.static(desktop_objects)
